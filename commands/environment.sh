a6e1ed315542a51a0ca0a234909702e0ddfa1516
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-12 19:18:38 -0400

    Removed requirements.txt export
    Signed-off-by: Taras Ivashchuk <taras@tarasivashchuk.com>

--- commands/environment.sh
+++ commands/environment.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source /Users/taras/Code/virtual-environments/marketml/bin/activate
+source /Users/taras/Code/virtual-environments/MarketML/bin/activate
 /Users/taras/.poetry/bin/poetry update
 /Users/taras/.poetry/bin/poetry install
-/Users/taras/.poetry/bin/poetry export -f "requirements.txt" -o "requirements.txt" --without-hashes
