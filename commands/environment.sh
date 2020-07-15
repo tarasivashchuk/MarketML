183a518cd12896653f379e951ea7ed28b3c4347e
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-15 08:46:41 -0400

    Removed path to poetry

--- commands/environment.sh
+++ commands/environment.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /Users/taras/Code/virtual-environments/MarketML/bin/activate
-/Users/taras/.poetry/bin/poetry update
-/Users/taras/.poetry/bin/poetry install
+poetry update
+poetry install
