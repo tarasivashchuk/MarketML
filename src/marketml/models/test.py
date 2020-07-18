bedd6c01455a1383dab827c3fda62ccedae39206
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-18 03:40:02 -0400

    Added logging and switched to Fire for CLI app

--- src/marketml/models/test.py
+++ src/marketml/models/test.py
@@ -1,9 +1,10 @@
 """CLI program for building, training, testing, and using models."""
-from typing import Dict, Any, List, Optional
 
-import click
+from datetime import datetime as dt
+from typing import Dict, Any, List, Optional, Union
+
+import fire
 import logzero
-import pendulum
 from tensorflow.python.keras.models import Model
 
 import marketml
@@ -14,12 +15,21 @@ from marketml.models.models import PricePredictor
 logger = logzero.setup_logger(__file__)
 
 
-@click.group()
-def test():
-    pass
+# @click.group()
+# def test():
+#     pass
 
 
-def setup_transformer(predictor: PricePredictor, num_attention_layers, num_attention_heads, attention_key_size, attention_value_size, attention_dense_size, output_dense_sizes, dropout):
+def setup_transformer(
+    predictor: PricePredictor,
+    num_attention_layers,
+    num_attention_heads,
+    attention_key_size,
+    attention_value_size,
+    attention_dense_size,
+    output_dense_sizes,
+    dropout,
+):
     params = {
         "num_attention_layers": num_attention_layers,
         "num_attention_heads": num_attention_heads,
@@ -27,43 +37,78 @@ def setup_transformer(predictor: PricePredictor, num_attention_layers, num_atten
         "output_dense_sizes": output_dense_sizes,
         "dropout": dropout,
     }
+    logger.debug(f"Model Parameters: {params}")
     return predictor.build_transformer(params)
 
 
-@test.command("train")
-@click.option("--tickers", type=str, required=True)
-@click.option("--num_attn", type=int, required=True)
-@click.option("--num_heads", type=int, required=True)
-@click.option("--key_sz", type=int, required=True)
-@click.option("--value_sz", type=int, required=True)
-@click.option("--dense_sz", type=int, required=True)
-@click.option("--out_dense_szs", type=str, required=True)
-@click.option("--dropout", type=float, default=0.2)
-@click.option("--batch_sz", type=int, default=8)
-@click.option("--epochs", type=int, default=5)
-def train(tickers, num_attn: int, num_heads: int, key_sz, value_sz, dense_sz, out_dense_szs, dropout, batch_sz, epochs,) -> Model:
-    tickers = tickers.split(" ")
-    out_dense_szs = [int(sz) for sz in out_dense_szs.split(" ")]
+# @test.command("train")
+# @click.option("--num_attention", type=int, required=True)
+# @click.option("--num_heads", type=int, required=True)
+# @click.option("--key_size", type=int, required=True)
+# @click.option("--value_size", type=int, required=True)
+# @click.option("--dense_size", type=int, required=True)
+# @click.option("--out_dense_sizes", type=str, required=True)
+# @click.option("--dropout", type=float, default=0.2)
+# @click.option("--batch_size", type=int, default=8)
+# @click.option("--epochs", type=int, default=5)
+def train(
+    ticker: str,
+    num_attention: int,
+    num_heads: int,
+    key_size: int,
+    value_size: int,
+    dense_size: int,
+    out_dense_sizes: Union[List[int], str],
+    dropout: float,
+    batch_size: int,
+    epochs: int,
+) -> Model:
+    out_dense_sizes = (
+        [int(size) for size in out_dense_sizes.split(" ")]
+        if isinstance(out_dense_sizes, str)
+        else out_dense_sizes
+    )
 
-    logger.debug(tickers)
-    logger.debug(num_attn)
-    logger.debug(num_heads)
-    logger.debug(key_sz)
-    logger.debug(value_sz)
-    logger.debug(dense_sz)
-    logger.debug(out_dense_szs)
-    logger.debug(dropout)
-    logger.debug(batch_sz)
-    logger.debug(epochs)
+    logger.info("Setting up dataset...")
     data_loader = StockPreprocessor()
-    [data_loader(ticker) for ticker in tickers]
+    data_loader(ticker)
     train_data, test_data, validation_data = data_loader.process()
-    print(train_data[0].shape)
+    logger.info("Dataset built!")
 
+    logger.info("Building model...")
     model_builder = PricePredictor((train_data[0].shape[1], train_data[0].shape[2]))
-    model = setup_transformer(model_builder, num_attn, num_heads, key_sz, value_sz, dense_sz, out_dense_szs, dropout)
-    model.fit(train_data[0], train_data[1], batch_size=batch_sz, epochs=epochs, validation_data=(validation_data[0], validation_data[1]), shuffle=False)
-    model.save(marketml.project_dir.joinpath(f"models/{pendulum.now()}.hdf5"))
+    model = setup_transformer(
+        model_builder,
+        num_attention,
+        num_heads,
+        key_size,
+        value_size,
+        dense_size,
+        out_dense_sizes,
+        dropout,
+    )
+    logger.info("Model built!")
+
+    logger.info("Training model...")
+    model.fit(
+        train_data[0],
+        train_data[1],
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(validation_data[0], validation_data[1]),
+        shuffle=False,
+    )
+    logger.info("Model trained!")
+
+    logger.info("Evaluating performance...")
+    performance = model.evaluate(
+        test_data[0], test_data[1], batch_size=batch_size, return_dict=True
+    )
+    [logger.info(f"{metric.title()}: {value}") for metric, value in performance.items()]
+
+    logger.info("Exporting model...")
+    model_filename = f"{performance['loss']}-{dt.now()}.hdf5"
+    model.save(marketml.project_dir.joinpath(f"models/{model_filename}"))
 
     return model
 
@@ -72,6 +117,7 @@ def train(tickers, num_attn: int, num_heads: int, key_sz, value_sz, dense_sz, ou
     # TODO: Add logging
     # TODO: Integrate with Darts as a forecasting model
 
-if __name__ == '__main__':
-    # python models/test.py train --num_attn 5 --num_heads 12 --key_sz 128 --value_sz 64 --dense_sz 128 --out_dense_szs "128 64" --tickers "AAPL MSFT AMZN"
-    test()
+
+if __name__ == "__main__":
+    # python models/test.py train --num_attention 5 --num_heads 12 --key_size 128 --value_size 64 --dense_size 128 --out_dense_sizes "128 64"
+    fire.Fire(name="MarketML Model Tests")
